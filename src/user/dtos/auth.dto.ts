1185332f91509fb7f1da854726fcecd933590a2a
kennedy7 <kennedyhillary6@gmail.com>
2023-06-18 12:01:04 +0100

    auth dto validator

--- src/user/dtos/auth.dto.ts
+++ src/user/dtos/auth.dto.ts
@@ -1,10 +1,23 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Matches,
+} from 'class-validator';
 
 export class SignUpDto {
   @IsString()
   @IsNotEmpty()
   name: string;
+
+  @Matches(/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/)
   phone: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(5)
   password: string;
 }

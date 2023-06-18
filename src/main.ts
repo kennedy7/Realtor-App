b99c06f9c68109a1dcc33ef7c47e24273c2dab3e
kennedy7 <kennedyhillary6@gmail.com>
2023-06-18 12:09:15 +0100

    enabling useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //enabling globalPipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //helps keep the dto very strict to avoid update with non-dto parameter
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

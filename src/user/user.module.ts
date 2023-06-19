0586dbcb691ecedbc88ebe93750db535eab2118e
kennedy7 <kennedyhillary6@gmail.com>
2023-06-19 12:38:59 +0100

    prismaService Export

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class UserModule {}

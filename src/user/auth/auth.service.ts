9e131f93b60fce93ea6f124421477b9105c67537
kennedy7 <kennedyhillary6@gmail.com>
2023-06-20 12:35:10 +0100

    unique email

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SignUpDto } from '../dtos/auth.dto';
 
+interface SignUpParams {
+  email: string;
+  phone: string;
+  name: string;
+  password: string;
+}
 @Injectable()
 export class AuthService {
   constructor(private readonly primaService: PrismaService) {}
-  signup() {}
+  async signup({email}: SignUpDto) {
+    const userExists = this.primaService.user.findUnique({where: {email}})
+  }
 }

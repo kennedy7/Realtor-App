5633c2c244dd2a503adedef830bfac9ac88ada4c
kennedy7 <kennedyhillary6@gmail.com>
2023-06-21 19:03:50 +0100

    password hash

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignUpDto } from '../dtos/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 interface SignUpParams {
   email: string;
@@ -11,12 +12,14 @@ interface SignUpParams {
 @Injectable()
 export class AuthService {
   constructor(private readonly primaService: PrismaService) {}
-  async signup({ email }: SignUpParams) {
+  async signup({ email, password }: SignUpDto) {
     const userExists = await this.primaService.user.findUnique({
       where: { email },
     });
     if (userExists) {
       throw new ConflictException();
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log({ hashedPassword });
   }
 }

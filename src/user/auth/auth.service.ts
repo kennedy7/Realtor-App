613cd30fb14782eba41fa1274b4282350de8e3e7
kennedy7 <kennedyhillary6@gmail.com>
2023-06-28 07:06:50 +0100

    Generateproductkey

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -69,7 +69,7 @@ export class AuthService {
     return { AccessToken };
   }
   generateProductKey(email: string, usertype: userType) {
-    const string = `${email}-${userType}-${process.env.PRODUCT_SECRET_KEY}`;
+    const string = `${email}-${usertype}-${process.env.PRODUCT_SECRET_KEY}`;
     const key = bcrypt.hash(string, 10);
     return key;
   }

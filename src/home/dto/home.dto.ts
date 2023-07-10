db37a553e292ce191d625d774078159a609f3efb
kennedy <kennedyhillary6@gmail.com>
2023-07-10 21:09:08 +0100

    refactor

--- src/home/dto/home.dto.ts
+++ src/home/dto/home.dto.ts
@@ -1,1 +1,19 @@
+import { PropertyType } from "@prisma/client";
+
 export class CreateHomeDto {}
+
+export class HomeResponseDto{
+    id: number;
+    address: string;
+    number_of_bedrooms: number;
+    number_of_bathrooms: number;
+    city: string;
+    listed_date: Date;
+    price: number;
+    land_size: number;
+    propertyType: PropertyType;
+    createdAt: Date;
+    updatedAt: Date;
+    realtor
+
+}

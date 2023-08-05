a70d6304831e61e8f0effa0d4ba5a00878527db0
kennedy <kennedyhillary6@gmail.com>
2023-08-05 15:17:38 +0100

    refactor home-dto

--- src/home/dto/home.dto.ts
+++ src/home/dto/home.dto.ts
@@ -1,19 +1,32 @@
-import { PropertyType } from "@prisma/client";
-
+import { PropertyType } from '@prisma/client';
+import { Exclude, Expose } from 'class-transformer';
 export class CreateHomeDto {}
 
-export class HomeResponseDto{
-    id: number;
-    address: string;
-    number_of_bedrooms: number;
-    number_of_bathrooms: number;
-    city: string;
-    listed_date: Date;
-    price: number;
-    land_size: number;
-    propertyType: PropertyType;
-    createdAt: Date;
-    updatedAt: Date;
-    realtor
+export class HomeResponseDto {
+  id: number;
+  address: string;
 
+  @Exclude()
+  number_of_bedrooms: number;
+
+  @Expose({ name: 'numberOfBedrooms' })
+  numberOfBedrooms() {
+    return this.number_of_bedrooms;
+  }
+
+  @Exclude()
+  number_of_bathrooms: number;
+
+  @Expose({ name: 'numberOfBathrooms' })
+  numberOfBathrooms() {
+    return this.number_of_bathrooms;
+  }
+  city: string;
+  listed_date: Date;
+  price: number;
+  land_size: number;
+  propertyType: PropertyType;
+  createdAt: Date;
+  updatedAt: Date;
+  realtor_id: number;
 }

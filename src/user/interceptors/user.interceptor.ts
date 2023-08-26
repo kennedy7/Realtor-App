f358ff89bab9d00f33f336b293eb232d496424dc
kennedy <kennedyhillary6@gmail.com>
2023-08-26 20:33:45 +0100

    custom user interceptor

--- src/user/interceptors/user.interceptor.ts
+++ src/user/interceptors/user.interceptor.ts
@@ -1,9 +1,12 @@
 import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 
 export class UserInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, handler: CallHandler) {
+  async intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
     const token = request?.headers?.authorization?.split('Bearer ')[1];
+    const user = await jwt.decode(token);
+    request.user = user;
 
     return handler.handle();
   }

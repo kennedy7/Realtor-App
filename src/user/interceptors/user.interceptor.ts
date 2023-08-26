2a877412106c08522e233bdbddcf8dabf5cdbe8a
kennedy <kennedyhillary6@gmail.com>
2023-08-26 20:24:44 +0100

    custom user interceptor

--- src/user/interceptors/user.interceptor.ts
+++ src/user/interceptors/user.interceptor.ts
@@ -3,6 +3,8 @@ import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 export class UserInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
+    const token = request?.headers?.authorization?.split('Bearer ')[1];
+
     return handler.handle();
   }
 }

4e419ef22750ae0002de06e3f582db1a14756dd0
kennedy <kennedyhillary6@gmail.com>
2023-08-31 19:34:45 +0100

    AuthGuard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,10 +1,15 @@
-import { CanActivate } from '@nestjs/common';
+import { CanActivate, ExecutionContext } from '@nestjs/common';
 
 //allows us access metadata
 import { Reflector } from '@nestjs/core';
 
 export class AuthGuard implements CanActivate {
-  canActivate() {
+  constructor(private readonly reflector: Reflector) {}
+  canActivate(context: ExecutionContext) {
+    const roles = this.reflector.getAllAndOverride('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     return true;
   }
 }

3e1f772af4e31b3c18067c97633fa6dfc530c05b
kennedy <kennedyhillary6@gmail.com>
2023-09-01 22:29:37 +0100

    roles check

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -6,10 +6,13 @@ import { Reflector } from '@nestjs/core';
 export class AuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext) {
+    //Getting all the roles from the metadata and replacing the request role
     const roles = this.reflector.getAllAndOverride('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
+    if (roles.length) {
+    }
     return true;
   }
 }

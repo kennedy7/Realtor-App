1ce5a11357b0f747667d93f4aa5b73b614797532
kennedy <kennedyhillary6@gmail.com>
2023-09-02 18:41:34 +0100

    gettokenFrom header

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -12,6 +12,8 @@ export class AuthGuard implements CanActivate {
       context.getClass(),
     ]);
     if (roles.length) {
+      const request = context.switchToHttp().getRequest();
+      const token = request.headers?.authorization?.split('Bearer ')[1];
     }
     return true;
   }
